dff24c6f1782fb7c317dddd143ce186086312385
Franco Catalano <franco.catalano@appfolio.com>
2019-09-10 13:36:26 -0700

    FC - Remove unused ApplicationController code and tests

--- test/controllers/application_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ApplicationControllerTest < ActionDispatch::IntegrationTest
-  test 'should get home' do
-    get root_path
-    assert_response :success
-    assert_select 'h1', 'Hello World'
-  end
-end
