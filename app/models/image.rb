74d732c641e485c4889662c2d7ff90903a789bcc
Franco Catalano <franco.catalano@appfolio.com>
2019-09-10 11:32:27 -0700

    FC - Rubocop fixes

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,3 @@
 class Image < ApplicationRecord
-    validates :url, format: {with: URI::regexp(%w(http https))}
+  validates :url, format: { with: URI.regexp(%w[http https]) }
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,54 +2,54 @@ require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @validImage= images(:valid)
-    @invalidImage= images(:invalid)
+    @valid_image = images(:valid)
+    @invalid_image = images(:invalid)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get images_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get new_image_url
     assert_response :success
   end
 
-  test "should create image" do
+  test 'should create image' do
     assert_difference('Image.count') do
-      post images_url, params: { image: { url: @validImage.url } }
+      post images_url, params: { image: { url: @valid_image.url } }
     end
 
     assert_redirected_to image_url(Image.last)
   end
 
-  test "should not create image" do
+  test 'should not create image' do
     assert_no_difference('Image.count') do
-      post images_url, params: { image: { url: @invalidImage.url } }
+      post images_url, params: { image: { url: @invalid_image.url } }
     end
 
-    assert_select "span", "is invalid"
+    assert_select 'span', 'is invalid'
   end
 
-  test "should show image" do
-    get image_url(@validImage)
+  test 'should show image' do
+    get image_url(@valid_image)
     assert_response :success
   end
 
-  test "should get edit" do
-    get edit_image_url(@validImage)
+  test 'should get edit' do
+    get edit_image_url(@valid_image)
     assert_response :success
   end
 
-  test "should update image" do
-    patch image_url(@validImage), params: { image: { url: @validImage.url } }
-    assert_redirected_to image_url(@validImage)
+  test 'should update image' do
+    patch image_url(@valid_image), params: { image: { url: @valid_image.url } }
+    assert_redirected_to image_url(@valid_image)
   end
 
-  test "should destroy image" do
+  test 'should destroy image' do
     assert_difference('Image.count', -1) do
-      delete image_url(@validImage)
+      delete image_url(@valid_image)
     end
 
     assert_redirected_to images_url
