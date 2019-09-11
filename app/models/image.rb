baa6a152e3db2251f8c7e368e980842e6d5acb80
Franco Catalano <franco.catalano@appfolio.com>
2019-09-11 14:17:07 -0700

    FC - Add tag searching

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,12 @@
 class Image < ApplicationRecord
   validates :url, format: { with: %r{https?:\/\/[\S]+\.[\S]+\.[\S]+\/([\S]{3,4})} }
   acts_as_taggable
+
+  def self.filter(tag)
+    if tag&.present?
+      Image.tagged_with(tag)
+    else
+      Image.all
+    end
+  end
 end
