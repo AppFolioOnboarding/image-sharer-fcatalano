5567c3cf8a9dbd7d4ae22d032dfb235ae5bc855e
Franco Catalano <franco.catalano@appfolio.com>
2019-09-11 17:16:53 -0700

    FC - Implement form, footer, and tests

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Feedback from './Feedback';
+import Footer from './Footer';
 
 export default function App() {
   return (
     <div>
       <Header title="Tell us what you think" />
-      Put your components here: Flash Message, Form, Footer
+      <Feedback />
+      <Footer />
     </div>
   );
 }
