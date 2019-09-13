e21790d117aa81aead1b144bed2f1eb126a96d05
Franco Catalano <franco.catalano@appfolio.com>
2019-09-12 17:43:36 -0700

    FC - Add feedback submission

--- app/controllers/api/feedbacks_controller.rb
+++ app/controllers/api/feedbacks_controller.rb
@@ -1,7 +1,19 @@
 module Api
   class FeedbacksController < ApplicationController
     def create
-      # Implement your create action
+      @feedback = Feedback.new(feedback_params)
+
+      if @feedback.save
+        render json: { message: 'Success: Comments saved successfully!' }, status: :created
+      else
+        render json: { message: 'Error: name and comment are required!' }, status: :unprocessable_entity
+      end
+    end
+
+    private
+
+    def feedback_params
+      params.require(:feedback).permit(:name, :comments)
     end
   end
 end
